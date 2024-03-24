6df58b5e192c9b052188f4da6c4ccc084cd11c9d
Keane Asson <keane@asson.com.au>
2024-03-25 00:25:37 +1000

    Added more files related to previous commmit

--- src/main/java/com/example/assessment/MainController.java
+++ src/main/java/com/example/assessment/MainController.java
@@ -1,2 +1,33 @@
-package com.example.assessment;public class MainController {
+package com.example.assessment;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainController {
+
+    public static final int WIDTH = 600;
+
+    public static final int HEIGHT = 400;
+
+    @FXML
+    private Menu logout_menu;
+
+    @FXML
+    private Text title;
+
+    @FXML
+    protected void logoutClicked() throws IOException {
+        // Will need to change the page here NOT IMPLEMENTED
+        Stage stage = (Stage) title.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("startup_view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), HelloApplication.WIDTH, HelloApplication.HEIGHT);
+        stage.setScene(scene);
+    }
+
 }
--- src/main/java/com/example/assessment/SignupController.java
+++ src/main/java/com/example/assessment/SignupController.java
@@ -1,4 +1,36 @@
 package com.example.assessment;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class SignupController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private Button login_btn;
+
+    @FXML
+    protected void loginClicked() throws IOException {
+        // Will need to change the page here NOT IMPLEMENTED
+        Stage stage = (Stage) login_btn.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("startup_view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), HelloApplication.WIDTH, HelloApplication.HEIGHT);
+        stage.setScene(scene);
+    }
+
 }
