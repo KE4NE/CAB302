6598ac742ed665277cdebad5063ef59f7f262d87
Simon Wegener <simonwegener0@gmail.com>
2024-04-25 11:39:19 +1000

    initial settings page

---
+++ src/main/java/com/example/assessment/controllers/SettingsController.java
@@ -0,0 +1,15 @@
+package com.example.assessment.controllers;
+
+import com.example.assessment.HelloApplication;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static com.example.assessment.SceneHelper.setScene;
+public class SettingsController {
+}
