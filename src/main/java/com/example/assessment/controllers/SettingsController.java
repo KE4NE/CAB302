09ec059a61c04320d8cb6b5557ff7eafd6f43fac
Simon Wegener <simonwegener0@gmail.com>
2024-04-25 12:07:21 +1000

    settings page and icon

--- src/main/java/com/example/assessment/controllers/SettingsController.java
+++ src/main/java/com/example/assessment/controllers/SettingsController.java
@@ -2,14 +2,118 @@ package com.example.assessment.controllers;
 
 import com.example.assessment.HelloApplication;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
 
 import java.io.IOException;
 
 import static com.example.assessment.SceneHelper.setScene;
+
 public class SettingsController {
+
+    public static final int WIDTH = 845;
+
+    public static final int HEIGHT = 560;
+
+    @FXML
+    private Button logout_btn;
+
+    @FXML
+    private HBox calendar_hbox;
+
+    @FXML
+    private HBox stats_hbox;
+
+    @FXML
+    private Button stats_btn;
+
+    @FXML
+    private HBox logout_hbox;
+
+    @FXML
+    private Button calendar_btn;
+
+
+    private boolean calendarBtnBool;
+
+    public void initialize() {
+        calendarSelected();
+    }
+
+    @FXML
+    protected void logoutClicked() throws IOException {
+        Stage stage = (Stage) logout_btn.getScene().getWindow();
+        setScene(stage, "startup_view.fxml", HelloApplication.WIDTH, HelloApplication.HEIGHT);
+        stage.centerOnScreen();
+    }
+
+    @FXML
+    protected void calendarSelected() {
+        calendarBtnBool = true;
+        calendar_hbox.setStyle("-fx-background-color:#5F7882; -fx-border-color: black; -fx-border-width:1 0 1 0");
+        calendar_btn.setStyle("-fx-background-color:#5F7882;");
+
+        stats_hbox.setStyle("-fx-background-color:#C7D4D9; -fx-border-color: black; -fx-border-width:0 0 1 0");
+        stats_btn.setStyle("-fx-background-color: #C7D4D9");
+    }
+
+    @FXML
+    protected void statisticsSelected() {
+        calendarBtnBool = false;
+        stats_hbox.setStyle("-fx-background-color:#5F7882; -fx-border-color: black; -fx-border-width:0 0 1 0");
+        stats_btn.setStyle("-fx-background-color:#5F7882;");
+
+        calendar_hbox.setStyle("-fx-background-color:#C7D4D9; -fx-border-color: black; -fx-border-width:1 0 1 0");
+        calendar_btn.setStyle("-fx-background-color:#C7D4D9;");
+    }
+
+    @FXML
+    protected void hoveredCalendarBtn() {
+        if (!calendarBtnBool) {
+            calendar_hbox.setStyle("-fx-background-color:#74A7BB; -fx-border-color: black; -fx-border-width:1 0 1 0");
+            calendar_btn.setStyle("-fx-background-color:#74A7BB;");
+        }
+
+    }
+
+    @FXML
+    protected void exitedCalendarBtn() {
+        if (!calendarBtnBool) {
+            calendar_hbox.setStyle("-fx-background-color:#C7D4D9; -fx-border-color: black; -fx-border-width:1 0 1 0");
+            calendar_btn.setStyle("-fx-background-color:#C7D4D9;");
+        }
+    }
+
+    @FXML
+    protected void hoveredStatsBtn() {
+        if (calendarBtnBool) {
+            stats_hbox.setStyle("-fx-background-color:#74A7BB; -fx-border-color: black; -fx-border-width:0 0 1 0");
+            stats_btn.setStyle("-fx-background-color:#74A7BB;");
+        }
+
+    }
+
+    @FXML
+    protected void exitedStatsBtn() {
+        if (calendarBtnBool) {
+            stats_hbox.setStyle("-fx-background-color:#C7D4D9; -fx-border-color: black; -fx-border-width:0 0 1 0");
+            stats_btn.setStyle("-fx-background-color:#C7D4D9;");
+        }
+    }
+    @FXML
+    protected void hoveredLogoutBtn() {
+        logout_hbox.setStyle("-fx-background-color:#74A7BB; -fx-border-color: black; -fx-border-width:0 0 1 0");
+        logout_btn.setStyle("-fx-background-color:#74A7BB;");
+    }
+
+    @FXML
+    protected void exitedLogoutBtn() {
+        logout_hbox.setStyle("-fx-background-color:#C7D4D9; -fx-border-color: black; -fx-border-width:0 0 1 0");
+        logout_btn.setStyle("-fx-background-color:#C7D4D9;");
+    }
+
+
 }
