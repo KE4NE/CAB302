eded95f5000c0245850597d16113970d5ccf0b48
css10212477 <cssirgunas@hotmail.com>
2024-05-14 13:54:12 +1000

    attempt to add T&C txt file to display

--- src/main/java/com/example/assessment/controllers/TermsConditionsController.java
+++ src/main/java/com/example/assessment/controllers/TermsConditionsController.java
@@ -3,7 +3,12 @@ package com.example.assessment.controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
 import javafx.scene.text.Text;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class TermsConditionsController {
 
@@ -19,8 +24,26 @@ public class TermsConditionsController {
     @FXML
     private Text title;
 
+    @FXML
+    private TextArea content;
+
     //function to main menu, open view of main menu when accepted button pressed
 
     //function to take back to log in, if decline button pressed, open start up view
+
+    //function to load text file containing Ts&Cs, and append each line to ScrollPane
+    public void initialize(){
+        try{
+            BufferedReader in = new BufferedReader(new FileReader("Terms_and_Conditions.txt"));
+            String str;
+            while ((str = in.readLine()) != null){
+                System.out.println(str);
+                content.appendText(str);
+            }
+            in.close();
+        }
+        catch (IOException ioe) {}
+
+    }
 
 }
